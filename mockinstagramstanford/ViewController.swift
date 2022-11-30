bec2ad08e59b95401c3ebd0f3011832077aca2d8
isaacwizard1 <106562755+isaacwizard1@users.noreply.github.com>
2022-11-29 22:52:28 -0500

    Initial Commit

---
+++ mockinstagramstanford/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mockinstagramstanford
+//
+//  Created by isaac on 11/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
